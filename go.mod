c97c2babd824a44a73a1d993c02d1c0cca41fe02
jais marine <jaismarine@MacBook-Pro-jais.local>
2023-03-04 18:25:59 +0700

    feat: add first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module logger-test
+
+go 1.18
+
+require github.com/jaisanas/go-print v1.2.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import logger "github.com/jaisanas/go-print"
+
+func main()  {
+	logger.LogInfo("test 1")
+	logger.LogWarn("test 2")
+	logger.LogError("test 3")
+}
